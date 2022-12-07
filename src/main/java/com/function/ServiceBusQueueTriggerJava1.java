b9dfd0c78c738c5c1614028a68a3b189c3096f97
jdvcDev <jason.cook@microsoft.com>
2022-12-06 22:16:07 -0700

    initial commit

---
+++ src/main/java/com/function/ServiceBusQueueTriggerJava1.java
@@ -0,0 +1,22 @@
+package com.function;
+
+import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.*;
+
+/**
+ * Azure Functions with Service Bus Trigger.
+ */
+public class ServiceBusQueueTriggerJava1 {
+    /**
+     * This function will be invoked when a new message is received at the Service Bus Queue.
+     */
+    @FunctionName("ServiceBusQueueTriggerJava1")
+    public void run(
+            @ServiceBusQueueTrigger(name = "message", queueName = "processedsymbols", connection = "demolcsappdevsymboldemosb1_SERVICEBUS") String message,
+            final ExecutionContext context
+    ) 
+    {
+        context.getLogger().info("Java Service Bus Queue trigger function executed.");
+        context.getLogger().info(message);
+    }
+}
