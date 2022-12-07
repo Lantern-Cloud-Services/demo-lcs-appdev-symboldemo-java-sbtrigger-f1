b9b7c9c08abcd71aa132c3d25c3d1f3a4e205ce5
jdvcDev <jason.cook@microsoft.com>
2022-12-06 22:42:50 -0700

    cosmos persistence

--- src/main/java/com/function/ServiceBusQueueTriggerJava1.java
+++ src/main/java/com/function/ServiceBusQueueTriggerJava1.java
@@ -7,16 +7,39 @@ import com.microsoft.azure.functions.*;
  * Azure Functions with Service Bus Trigger.
  */
 public class ServiceBusQueueTriggerJava1 {
+/*
+    class SymbolDeltaHelper
+    {
+        public String symbol;
+        public String delta;
+        public String curValue;
+        public String origOrder;
+        public String procTimeStamp;
+    }
+*/
     /**
      * This function will be invoked when a new message is received at the Service Bus Queue.
      */
     @FunctionName("ServiceBusQueueTriggerJava1")
     public void run(
-            @ServiceBusQueueTrigger(name = "message", queueName = "processedsymbols", connection = "demolcsappdevsymboldemosb1_SERVICEBUS") String message,
+            @ServiceBusQueueTrigger(
+                name = "message", 
+                queueName = "processedsymbols", 
+                connection = "demolcsappdevsymboldemosb1_SERVICEBUS"
+                ) String message,
+            @CosmosDBOutput(
+                name = "databaseOutput",
+                databaseName = "SymbolProcessor",
+                collectionName = "ProcessedEvents",
+                connectionStringSetting = "CosmosDBConnectionString"
+                ) OutputBinding<String> document,
             final ExecutionContext context
     ) 
     {
         context.getLogger().info("Java Service Bus Queue trigger function executed.");
-        context.getLogger().info(message);
+        context.getLogger().info("Message received: " + message);
+
+        document.setValue(message);        
+        context.getLogger().info("Document persisted to cosmos: " + message);
     }
 }
